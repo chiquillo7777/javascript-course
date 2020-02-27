bf965cace39816ce195376685608228aedba9d5f
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-02-27 11:08:24 -0600

    first commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,17 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+var fullAge =  true;
+
+console.log(fullAge);
+
+var job;
+
+console.log(job);
+
+var joc, ismarried;
+
+job = 'teacher';
+ismarried = false;
