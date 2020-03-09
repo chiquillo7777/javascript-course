c94e258962febcbe0936381c07924c17dd77b402
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-03-09 14:34:06 -0600

    trying to create a js project with npm

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -14,3 +14,36 @@ var john = new SmithPerson('John', 1990);
 
 
 
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+//ES6
+
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge(){
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+
+    static greeting(){
+        console.log('Hey there!!');
+    }
+}
+
+const john6 = new Person6('John', 1990, 'Teacher');
+
+Person6.greeting();
